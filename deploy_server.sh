12e76ad8887d5de0e723e8ce9f69f5eb683c3d4d
mo-mueller <nono.game@googlemail.com>
2018-06-14 21:01:30 +0200

    No commit message provided

--- deploy_server.sh
+++ deploy_server.sh
@@ -7,6 +7,7 @@ then
     cp -r build/* /var/www/vhosts/m--m.tech/httpdocs
     echo "Files moved to live"
 elif [ "$1" = "stage" ]
+then
     cp -r build/* /var/www/vhosts/euve260381.serverprofi24.de/slate.euve260381.serverprofi24.de
     echo "Files moved to stage"
 fi
