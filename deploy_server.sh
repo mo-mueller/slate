e8e4b37e0e72b3bed870000c7afa04a5be747899
mo-mueller <nono.game@googlemail.com>
2018-06-14 21:00:15 +0200

    No commit message provided

--- deploy_server.sh
+++ deploy_server.sh
@@ -5,6 +5,8 @@ bundle exec middleman build --clean
 if [ "$1" = "live" ]
 then
     cp -r build/* /var/www/vhosts/m--m.tech/httpdocs
+    echo "Files moved to live"
 elif [ "$1" = "stage" ]
     cp -r build/* /var/www/vhosts/euve260381.serverprofi24.de/slate.euve260381.serverprofi24.de
+    echo "Files moved to stage"
 fi
